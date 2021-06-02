8b642b52b2867cd043b7450a4171dae2aa650e38
vlad16666 <63139563+vlad16666@users.noreply.github.com>
2021-06-02 09:30:58 +0900

    Add files via upload

---
+++ bombController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bombController : MonoBehaviour
+{
+    float x;
+    float z;
+    float y;
+    Transform enemy_tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy_tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "ground" )
+        {
+            x = Random.Range(-8.7f,5f);
+            z = Random.Range(-19f,8f);
+            y = Random.Range(20f,25f);
+            Instantiate(gameObject,new Vector3(x,y,z),enemy_tr.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ spriteController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class spriteController : MonoBehaviour
+{
+    Transform cube_tr;
+    int score = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube_tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown("w"))
+        {
+            cube_tr.Translate(0,0,1f);
+        } 
+        if (Input.GetKeyDown("a"))
+        {
+            cube_tr.Translate(-1f,0,0);
+        } 
+        if (Input.GetKeyDown("s"))
+        {
+            cube_tr.Translate(0,0,-1f);
+        } 
+        if (Input.GetKeyDown("d"))
+        {
+            cube_tr.Translate(1f,0,0);
+        } 
+        if(score == 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "enemy")
+        {
+            //print("в нас попали!");
+            score = score - 20;
+            print(score);
+        }
+    }
+    
+}
